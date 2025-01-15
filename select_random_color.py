d8ab2fc7a19438f379f6bad6b65f9fe1aa4962bf
k.ishikawa <98593352+ishikawa096@users.noreply.github.com>
2025-01-15 17:34:47 +0000

    fix: more vivid colors

--- select_random_color.py
+++ select_random_color.py
@@ -1,5 +1,5 @@
 
 def select_random_color():
     import random
-    colors = ['#cd2425', '#1e1dae', '#1dad23', '#e7de23', '#a67b24']
+    colors = ['#ff0000', '#ff8c00', '#ffff00', '#00ff00', '#0000ff']
     return random.choice(colors)
